5b204561731163acd2654fa825852b7fd544d2d1
Lyu.Hiroyama <lyuhiroyama@gmail.com>
2023-09-09 00:25:50 +0900

    Initial commit

--- MultiplicationQuiz-App/ContentView.swift
+++ MultiplicationQuiz-App/ContentView.swift
@@ -8,15 +8,85 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var speed = 5.0
+    @State private var isEditing = false
+    @State private var pickerSelection = 1
+    @State private var playMode = false
+    @State private var pushAnimationAmount = 1.0
+
+//text your multpilcation
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
+    
+        ZStack {
+            
+            VStack {
+                
+                Spacer()
+                
+                Text("Multiples of:")
+                Text("\(speed.formatted())")
+                    .font(.system(size:100).weight(.bold))
+                
+                
+                Slider(
+                    value: $speed,
+                    in: 0...20,
+                    step: 1
+                ) {
+                    Text("Speed")
+                } minimumValueLabel: {
+                    Text("0")
+                } maximumValueLabel: {
+                    Text("20")
+                } onEditingChanged: { editing in
+                    isEditing = editing
+                }
+                
+                Text("# of Questions:")
+                
+                Picker("Number of questions", selection: $pickerSelection) {
+                    Text("5").tag(0)
+                    Text("10").tag(1)
+                    Text("15").tag(2)
+                }//Picker
+                .pickerStyle(.segmented)
+                
+                Spacer()
+                
+                Button() {
+                    withAnimation {
+                        playMode.toggle()
+                    }
+                } label: {
+                    Image(systemName: "gamecontroller")
+                }
+                .font(.largeTitle)
+                .overlay(
+                    Circle()
+                        .stroke(.blue)
+                        .frame(width: 70, height: 70)
+                        .scaleEffect(playMode ? 2 : 1)
+                        .opacity(playMode ? 0 : 1)
+                )
+
+                Spacer()
+                
+            }//VStack
+            .padding()
+            
+            
+            if playMode {
+                Color.cyan
+                    .ignoresSafeArea()
+                    .transition(.opacity)
+            }
+            
+        }//ZStack
+
     }
+    
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
