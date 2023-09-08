94ace197be7f40706d4aaa53cae4ed2b382f41a2
Lyu.Hiroyama <lyuhiroyama@gmail.com>
2023-09-08 18:54:50 +0900

    Initial Commit

---
+++ MultiplicationQuiz-App/MultiplicationQuiz_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MultiplicationQuiz_AppApp.swift
+//  MultiplicationQuiz-App
+//
+//  Created by Lyu Hiroyama on 2023/09/08.
+//
+
+import SwiftUI
+
+@main
+struct MultiplicationQuiz_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
